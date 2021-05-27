55d714dc43fbb55d0ff52579032f0dabb77db0f2
Anders <and3k5@users.noreply.github.com>
2021-05-27 13:00:17 +0200

    Make test application

---
+++ TestApplication1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace TestApplication1
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.Write("Path for csv file: ");
+            var dataTable = AnotherCsvLib.Parse.ReadFileToDataTable(Console.ReadLine());
+            var data = new Dictionary<string, List<string>>();
+
+            var maxLengths = new Dictionary<string, int>();
+
+            foreach (DataColumn col in dataTable.Columns)
+            {
+                var values = new List<string>();
+                data.Add(col.ColumnName, values);
+                values.AddRange(from DataRow row in dataTable.Rows select row[col]?.ToString());
+
+                maxLengths.Add(col.ColumnName, values.Concat(new[] {col.ColumnName}).Max(x => x?.Length ?? 0));
+            }
+
+
+            const int margin = 2;
+
+            foreach (var value in data.Keys)
+            {
+                Console.Write(value);
+                Console.Write(new string(' ', maxLengths[value] + margin - value.Length));
+            }
+
+            Console.WriteLine();
+
+            var rowsCount = data.Values.GroupBy(x => x.Count).Single().Key;
+
+
+            for (var rowIndex = 0; rowIndex < rowsCount; rowIndex++)
+            {
+                foreach (var key in data.Keys)
+                {
+                    var value = data[key][rowIndex];
+                    Console.Write(value);
+                    Console.Write(new string(' ', maxLengths[key] + margin - value.Length));
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
